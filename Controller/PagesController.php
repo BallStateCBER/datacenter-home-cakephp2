4833a0d0593f852fd29cb2fccc430b06c0584c69
Graham Watson <graham@phantomwatson.com>
2015-05-06 14:36:53 -0400

    Refactor

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -314,51 +314,22 @@ class PagesController extends AppController {
 	}
 
 	private function __getSiteStatus($url) {
-		# initialise the CURL library
 		$ch = curl_init();
-
-		# specify the URL to be retrieved
 		curl_setopt($ch, CURLOPT_URL, $url);
-
-		# we want to get the contents of the URL and store it in a variable
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-		# specify the useragent: this is a required courtesy to site owners
-		//curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-
-		# ignore SSL errors
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-		# return headers as requested
-		if (true) {
-			curl_setopt($ch, CURLOPT_HEADER, 1);
-		}
-
-		/*
-		# only return headers
-		if ($headers == 'headers only') {
-			curl_setopt($ch, CURLOPT_NOBODY, 1);
-		}
-		*/
-
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // ignore SSL errors
+		curl_setopt($ch, CURLOPT_HEADER, 1);
 		$result = curl_exec($ch);
-
-		# free resources
 		curl_close($ch);
-
-		# send back the data
 		return $result;
 	}
 
 	public function statuscheck() {
-		$url = $_GET['url'];
-		$result = $this->__getSiteStatus($url);
-		$firstLine = substr($result, 0, strpos($result, "\n"));
-		$retval = array(
-			'status' => $firstLine, //stripos($firstLine, '200 OK') !== false,
+		$result = $this->__getSiteStatus($_GET['url']);
+        $this->set('result', array(
+			'status' => substr($result, 0, strpos($result, "\n")),
 			'debug' => stripos($result, 'debug-kit-toolbar') !== false
-		);
-        $this->set('result', $retval);
+		));
         $this->layout = 'json';
     }
 }
